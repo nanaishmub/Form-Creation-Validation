6ce26ca4c4654509d9fe3ff7ae0da4488cf30c20
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-07-24 09:04:15 +0100

    made corrections

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ document.addEventListener('DOMContentLoaded', (event) =>{
     form.addEventListener('submit', function(event){
         event.preventDefault();
 
-        const usernameInput = document.getElementById('username').value.trim();
-        const emailInput = document.getElementById('email').value.trim();
-        const passwordInput = document.getElementById('password').value.trim();
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
 
         let isValid = true;
 
         let messages = [];
 
-        if (usernameInput.length < 3){
+        if (username.length < 3){
             isValid = false;
             messages.push("Username must be more than 3 characters long");
         }
 
-        if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput)){
+        if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)){
             isValid = false;
             messages.push("Invalid email");
         }
 
-        if (passwordInput.length < 8) {
+        if (password.length < 8) {
             isValid = false;
             messages.push('Password must be at least 8 characters long');
         }
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', (event) =>{
         feedbackDiv.style.display = "block";
 
         if (isValid == true) {
-            feedbackDiv.textContent = "Registration Successful";
+            feedbackDiv.textContent = "Registration successful";
             feedbackDiv.style.color = "#28a745";
         }else {
             feedbackDiv.innerHTML = messages.join("<br>");
